2f50895a5454c5c23f59d155effa2ef772cbfe1f
MartinZakhaev <andikawahyusyaputra@gmail.com>
2024-11-12 13:52:33 +0700

    assets and metadata setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,14 @@ import React from "react";
 
 const Home = () => {
   return (
-    <h1 className="text-3xl font-black text-light-500">
-      Welcome to the world of Next.js 15!
-    </h1>
+    <>
+      <h1 className="text-3xl font-black text-light-500">
+        Welcome to the world of Next.js 15!
+      </h1>
+      <h1 className="font-space-grotesk text-3xl font-black text-light-500">
+        Welcome to the world of Next.js 15!
+      </h1>
+    </>
   );
 };
 
