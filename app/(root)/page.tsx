2193ca822f3cfea27931d199663b8df49997d4aa
MartinZakhaev <andikawahyusyaputra@gmail.com>
2024-11-14 19:33:38 +0700

    implement left & right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,5 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/route";
+import { auth } from "@/auth";
 
-// const Home = async () => {
 const Home = async () => {
   const session = await auth();
   console.log(session);
@@ -11,15 +8,6 @@ const Home = async () => {
       <h1 className="text-3xl font-black text-light-500">
         Welcome to the world of Next.js 15!
       </h1>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Logout</Button>
-      </form>
     </>
   );
 };
